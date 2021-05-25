0abad666d73384325d5deb694690e5ac79016660
sarahnvnt <47983951+sarahnvnt@users.noreply.github.com>
2021-05-25 12:19:31 +0700

    Semweb Upload

---
+++ public/result.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@100;400&family=Salsa&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+
+        <nav class="navbar ">
+            <div class="container-fluid d-flex">
+                <a href="index.html" class="navbar-brand">TI MatKul Repository</a>
+                <div class="">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Advanced Search</a>
+                    </li>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+
+    </div>
+
+    <div class="content-result">
+        <div class="search-container">
+            <form action="/action_page.php">
+                <input type="text" placeholder="Search the lesson here..." name="search">
+                <button type="submit">Search</button>
+            </form>
+        </div>
+
+        <div class="result">
+            <div class="card-result">
+                <p>Mata Kuliah : Semantik Web</p>
+                <p>Nama Dosen : Aditya Pradana S.T., M.Eng</p>
+                <p>Semester : 6</p>
+                <p>SKS : 3</p>
+                <p>Kode : D10G.62B2</p>
+                <p>Biodata Dosen : <a href="">link</a></p>
+            </div>
+            <div class="card-result">
+                <p>Mata Kuliah : Semantik Web</p>
+                <p>Nama Dosen : Aditya Pradana S.T., M.Eng</p>
+                <p>Semester : 6</p>
+                <p>SKS : 3</p>
+                <p>Kode : D10G.62B2</p>
+                <p>Biodata Dosen : <a href="">link</a> </p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
